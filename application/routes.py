0838f455da5b3da26549416c231aad18f1f0bdcb
Lincoln Dube <dube113@uwindsor.ca>
2020-05-05 19:56:40 -0400

    Welcome page v1 finished

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from application import app
 
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
 
 @app.route('/start')
+def start_login():
+    return 'hello'
