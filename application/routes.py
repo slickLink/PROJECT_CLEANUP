4bf58a0fab1e73f504ef6ed39b6899c09df02253
Lincoln Dube <dube113@uwindsor.ca>
2020-05-04 21:43:39 -0400

    added templates

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from application import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html', title='Home')
