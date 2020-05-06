90b32b5803cddd706cd796e73d1d89bbf53a64b5
Lincoln Dube <dube113@uwindsor.ca>
2020-05-05 22:51:31 -0400

    SpotifyOauth2 class created.

---
+++ application/oauth.py
@@ -0,0 +1,47 @@
+from application import app, utils
+import requests
+
+
+class SpotifyOauth2:
+    """This class is used to handles all authorization 
+    requirements needed by the Spotify Web API 
+    using the Authoriaztion Code Flow"""
+    
+    def __init__(self):
+        #
+        self.client_id = app.config['CLIENT_ID']
+        self.client_secret = app.config['CLIENT_SECRET']
+        self.redirect_uri = app.config['REDIRECT_URI']
+
+        def request_auth_url(self):
+            """ request authorization url,
+             returns the url or None """
+
+            SPOTIFY_AUTHORIZE_URL = 'https://accounts.spotify.com/authorize'
+
+            # protect against attacks such as cross-site request forgery using state
+            state = utils.random_string(10)
+            parameters = {
+                'client_id': self.client_id,
+                'response_type': 'code',
+                'redirect_uri': self.redirect_uri,
+                'state': state,
+                'scope': 'playlist-modify-private'
+            }
+
+            # request authorization code
+            response = requests.get(SPOTIFY_AUTHORIZE_URL, params=parameters)
+            
+            if response.status_code != 200:
+                return None
+            else:
+                return response.url
+
+            
+
+            
+           
+            
+
+
+
