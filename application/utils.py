9c46cf4de78d5a6ba8e4f3b183e8c178ccd21e79
slickLink <lincoln.dube@outlook.com>
2020-05-07 15:16:47 -0400

    encode_pair function

--- application/utils.py
+++ application/utils.py
@@ -1,9 +1,15 @@
 
-import random
+import random, base64
 
 def random_string(length):
     ''' Generates a random string of size (length)'''
 
     string = 'abcdefghijklmnopqrstuvwxyz0123456789'
     return ''.join(random.choice(string) for x in range(length))
+
+def encode_pair(first, second):
+    ''' returns a base64 encoded <first:second> string '''
+
+    encoded_data = base64.b64encode(bytes(f"{first}:{second}", "ISO-8859-1")).decode("ascii")
+    return encoded_data
 
