1d039521d87872a3baaa3a714d52efa51af93fc3
slickLink <lincoln.dube@outlook.com>
2020-05-07 14:52:36 -0400

    flash message for incorrect state

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, url_for, make_response, session
+from flask import render_template, redirect, request, url_for, make_response, session, flash
 from application import app
 from application.oauth import SpotifyOauth2
 
@@ -15,23 +15,26 @@ def start_login():
     auth_url_state = spotify_oauth.request_auth_url()
     if auth_url_state is None:
         return 'Sorry Will be fixed soon!'
-    else:
-        #save state in session
-        session['state'] = auth_url_state[1]
+
+    session['state'] = auth_url_state[1]
     return redirect(auth_url_state[0])
 
 @app.route('/callback')
 def callback():
     #check if user did not grant access 
     error_check = request.args.get('error') or None
-    if error_check is None:
+    if error_check:
         # send user back to welcome page
-        return redirect(url_for('start_login'))
+        return redirect(url_for('index'))
     
     # check for cross-site request forgery
     check_state = session.get('state')
     returned_state = request.args.get('state')
-    if check_state == returned_state:
-        return 'True'
-    else:
-        return 'False'
+    if check_state != returned_state:
+        # tell user of error
+        flash("incorrect state recieved from API request")
+        redirect(url_for('index'))
+    
+    #
+
+       
